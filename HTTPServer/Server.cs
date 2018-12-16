0d91b00f9349875c9b85f854c9b16ed1d9e1409e
maioy97 <mai.algendyfcis@gmail.com>
2018-12-16 17:39:05 +0200

    initial commit , server done

---
+++ HTTPServer/Server.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace HTTPServer
+{
+    class Server
+    {
+        Socket serverSocket;
+        int maxconnection;
+
+        public Server(int portNumber, string redirectionMatrixPath)
+        {
+            //TODO: call this.LoadRedirectionRules passing redirectionMatrixPath to it
+            this.LoadRedirectionRules(redirectionMatrixPath);
+            //TODO: initialize this.serverSocket
+            this.serverSocket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint IpE = new IPEndPoint(IPAddress.Parse("127.0.0.1"), portNumber);
+            serverSocket.Bind(IpE);
+            maxconnection = 100;
+        }
+
+        public void StartServer()
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(this.HandleConnection));
+            // TODO: Listen to connections, with large backlog.
+            serverSocket.Listen(maxconnection);
+            // TODO: Accept connections in while loop and start a thread for each connection on function "Handle Connection"
+            while (true)
+            {
+                //TODO: accept connections and start thread for each accepted connection.
+                thread.Start(serverSocket.Accept());
+            }
+        }
+
+        public void HandleConnection(object obj)
+        {
+
+            // TODO: Create client socket 
+            // set client socket ReceiveTimeout = 0 to indicate an infinite time-out period
+            Socket clientsoc = (Socket)obj;
+            clientsoc.ReceiveTimeout = 0;
+            int size = 0;
+            // TODO: receive requests in while true until remote client closes the socket.
+            while (true)
+            {
+                try
+                {
+                    byte[] message = new byte[1024];
+                    // TODO: Receive request
+                    size = clientsoc.Receive(message);
+                    // TODO: break the while loop if receivedLen==0
+                    if (size == 0) { break; }
+                    // TODO: Create a Request object using received request string
+                    Request req = new Request(Encoding.ASCII.GetString(message, 0, size));
+                    // TODO: Call HandleRequest Method that returns the response
+                    Response res = this.HandleRequest(req);
+                    // TODO: Send Response back to client
+                    message = Encoding.ASCII.GetBytes(res.ResponseString);
+                    clientsoc.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    // TODO: log exception using Logger class
+                    Logger.LogException(ex);
+                }
+            }
+            // TODO: close client socket
+            clientsoc.Close();
+        }
+
+        Response HandleRequest(Request request)
+        {
+            //throw new NotImplementedException();
+            string content ;
+            string contentpath;
+            string redirectionpath="";
+            StatusCode stC ;
+            try
+            {
+                //TODO: map the relativeURI in request to get the physical path of the resource.
+                contentpath = Configuration.RootPath + "\\" + request.relativeURI;
+                bool proper_request = request.ParseRequest();
+                //TODO: check for bad request 
+                if (!proper_request)
+                {
+                    stC = StatusCode.BadRequest;
+                    contentpath = Configuration.RootPath + '\\' + Configuration.NotFoundDefaultPageName;
+
+                }
+
+                //TODO: check for redirect
+                else if (Configuration.RedirectionRules.ContainsKey(request.relativeURI))
+                {
+                    stC = StatusCode.Redirect;
+                    redirectionpath = Configuration.RedirectionRules[request.relativeURI];
+                    contentpath = Configuration.RootPath + "\\" + redirectionpath;
+                }
+                //TODO: check file exists
+                else if (File.Exists(contentpath))
+                {
+                    // Create OK response
+                    stC = StatusCode.OK;
+                }
+                else
+                {
+                    stC = StatusCode.NotFound;
+                    contentpath = Configuration.RootPath + '\\' + Configuration.NotFoundDefaultPageName;
+                }
+                //TODO: read the physical file
+                content = File.ReadAllText(contentpath);
+            }
+            catch (Exception ex)
+            {
+                // TODO: log exception using Logger class
+                // TODO: in case of exception, return Internal Server Error. 
+                Logger.LogException(ex);
+                stC = StatusCode.InternalServerError;
+                contentpath = Configuration.RootPath + '\\' + Configuration.InternalErrorDefaultPageName;
+                content = File.ReadAllText(contentpath);
+            }
+            return new Response(stC, "html", content, redirectionpath);
+        }
+
+        private string GetRedirectionPagePathIFExist(string relativePath)
+        {
+            // using Configuration.RedirectionRules return the redirected page path if exists else returns empty
+            
+            return string.Empty;
+        }
+
+        private string LoadDefaultPage(string defaultPageName)
+        {
+            string filePath = Path.Combine(Configuration.RootPath, defaultPageName);
+            // TODO: check if filepath not exist log exception using Logger class and return empty string
+            
+            // else read file and return its content
+            return string.Empty;
+        }
+
+        private void LoadRedirectionRules(string filePath)
+        {
+            try
+            {
+                // TODO: using the filepath paramter read the redirection rules from file 
+                // then fill Configuration.RedirectionRules dictionary   aboutus.html,aboutus2.html
+                string[] redirectionline = File.ReadAllLines(filePath);
+                foreach (string line in redirectionline)
+                {
+                    string[] uri = line.Split(',');
+                    Configuration.RedirectionRules.Add(uri[0],uri[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: log exception using Logger class
+                Logger.LogException(ex);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
