da0223e89280f3db126b73d9c27eddbb4b9cc36c
maioy97 <mai.algendyfcis@gmail.com>
2018-12-16 21:24:18 +0200

    responce.cs done

--- HTTPServer/Response.cs
+++ HTTPServer/Response.cs
@@ -30,19 +30,37 @@ namespace HTTPServer
         List<string> headerLines = new List<string>();
         public Response(StatusCode code, string contentType, string content, string redirectoinPath)
         {
-            throw new NotImplementedException();
+            string statusLine = GetStatusLine(code);
             // TODO: Add headlines (Content-Type, Content-Length,Date, [location if there is redirection])
-
+            headerLines.Add("Content-Type: " + contentType);
+            headerLines.Add("Content-Length: " + content.Length);
+            headerLines.Add("Date: " + DateTime.Now);
+            if (code == StatusCode.Redirect)
+                headerLines.Add("Redirect: " + redirectoinPath);
 
             // TODO: Create the request string
-
+            responseString = statusLine;
+            foreach (string header in headerLines)
+            {
+                responseString += header + "\r\n";
+            }
+            responseString += "\r\n" + content;
         }
 
         private string GetStatusLine(StatusCode code)
         {
             // TODO: Create the response status line and return it
-            string statusLine = string.Empty;
+            string message = "ok";
+            if (code == StatusCode.BadRequest)
+                message = "Bad Request";
+            else if (code == StatusCode.InternalServerError)
+                message = "Internal Server Error";
+            else if (code == StatusCode.NotFound)
+                message = "Not Found";
+            else if (code == StatusCode.Redirect)
+                message = "Redirect";
 
+            string statusLine = "HTTP/1.1 " + code + ' ' + message + "\r\n";
             return statusLine;
         }
     }
